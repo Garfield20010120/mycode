d961299b327e58578e8cae5fb5db565700dc4163
Garfield20010102 <guojh2022@163.com>
2022-04-16 13:12:33 +0800

    22.4.15操作符-结构成员访问

--- 操作符/22.4.15操作符-结构成员访问.cpp
+++ 操作符/22.4.15操作符-结构成员访问.cpp
@@ -1,5 +1,6 @@
 //22.4.15操作符 结构成员访问（.和->） 
 #include<stdio.h>
+#include<string.h>
 struct Stu
 {
 	char name[20];
@@ -11,9 +12,12 @@ int main()
 {
 	int a = 0;
 	struct Stu S1 = {"郭子",21,"20220415"};
-//	S1.name = "鸽子";//"." 这里用strcpy 
-//	S1->age = 22;//"->"
+	struct Stu* ps = &S1; //要先声明 
+	strcpy(S1.name,"鸽子");//"." 这里用strcpy 
+	ps->age = 22;//"->"前要用指针不然会报错：[Error] base operand of '->' has non-pointer type 'Stu' 
 	printf("%s\n",S1.name);
 	printf("%d\n",S1.age);
+	printf("%s\n",ps->name);
+	printf("%d\n",(*ps).age);
 	return 0;
 }
